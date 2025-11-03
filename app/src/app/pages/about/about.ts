a1fe76436316faa95c2a3d9f0b5f23057b01adda
Saif8599 <78470780+Saif8599@users.noreply.github.com>
2025-11-03 16:39:25 +0100

    feat: add Home & About route pages

---
+++ app/src/app/pages/about/about.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TextBlock } from '../../components/text-block/text-block';
+
+@Component({
+  selector: 'app-about',
+  imports: [TextBlock],
+  templateUrl: './about.html',
+  styleUrl: './about.css'
+})
+export class About {
+
+}
---
+++ app/src/app/pages/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TextBlock } from '../../components/text-block/text-block';
+
+@Component({
+  selector: 'app-home',
+  imports: [TextBlock],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+
+}
