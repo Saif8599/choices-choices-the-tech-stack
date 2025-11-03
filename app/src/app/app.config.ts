591950be5f42b51b994c17799af839406be4acc3
Saif8599 <78470780+Saif8599@users.noreply.github.com>
2025-11-03 20:20:19 +0100

    feat: add hash location strategy for netlify

--- app/src/app/app.config.ts
+++ app/src/app/app.config.ts
@@ -1,13 +1,20 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    provideClientHydration(withEventReplay()),
+  ],
 };
